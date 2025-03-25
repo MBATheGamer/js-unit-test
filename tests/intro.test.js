65b9978df53d268e8bcf8f6620d857e397f31a1e
MBATheGamer <mbathegamer@gmail.com>
2025-03-25 16:13:15 +0100

    Test-Driven Development

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it, test } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { calculateAverage, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   test("Should return the first argument if it is greater.", () => {
@@ -38,5 +38,23 @@ describe("fizzBuzz", () => {
 
   test("Should return argument as a string if it is not divisible by 3 or 5.", () => {
     expect(fizzBuzz(1)).toBe("1");
+  });
+});
+
+describe("calculateAverage", () => {
+  it("Should return NaN if given an empty array.", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  it("Should calculate the average of an array with a single element.", () => {
+    expect(calculateAverage([7])).toBe(7);
+  });
+
+  it("Should calculate the average of an array with two elements.", () => {
+    expect(calculateAverage([7, 8])).toBe(7.5);
+  });
+
+  it("Should calculate the average of an array with three elements.", () => {
+    expect(calculateAverage([7, 8, 9])).toBe(8);
   });
 });
