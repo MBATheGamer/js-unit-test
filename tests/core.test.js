136ea6ed8207c25dfacaeef9db00ba82f474e905
MBATheGamer <mbathegamer@gmail.com>
2025-03-26 16:18:22 +0100

    Parameterized tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -247,3 +247,20 @@ describe("canDrive", () => {
     expect(canDrive(18, "UK")).toBe(true);
   });
 });
+
+describe("canDrive with each", () => {
+  it.each([
+    { age: 15, country: "US", result: false },
+    { age: 16, country: "US", result: true },
+    { age: 17, country: "US", result: true },
+    { age: 16, country: "UK", result: false },
+    { age: 17, country: "UK", result: true },
+    { age: 18, country: "UK", result: true },
+  ])("Should return $result for $age, $country", ({ age, country, result }) => {
+    expect(canDrive(age, country)).toBe(result);
+  });
+
+  it("Should return an error for invalid country code.", () => {
+    expect(canDrive(16, "UZ")).toMatch(/invalid/i);
+  });
+});
